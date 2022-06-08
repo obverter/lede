aa88c3b118a24b758606569050db11c8c56fe9c3
obverter <ben@obverter.com>
2022-06-08 15:10:17 -0700

    Complete part one, high-five self

--- homework/homework-2-part1-elliott.py
+++ homework/homework-2-part1-elliott.py
@@ -42,26 +42,67 @@ if median(numbers) > mean(numbers):
     )
 else:
     print(
-        f"The mean of the list is larger than the median. ({mean(numbers)} > {median(numbers)})\n"
+        f"The mean of the list is larger than the median. ({int(mean(numbers))} > {int(median(numbers))})\n\n"
     )
-
+print("✪ ✪ ✪ ✪ ✪ ✪ ✪ ✪ ✪ ✪\n\n")
 # PART ONE: Dictionaries
 
 # 1) Sometimes dictionaries are used to describe multiple aspects of a single object. Like, say, a movie. Define a dictionary called movie that works with the following code.
 
-# print("My favorite movie is", movie['title'], "which was released in", movie['year'], "and was directed by", movie['director'])
+movie = {
+    "title": "Gigli",
+    "year": 2003,
+    "director": "Martin Brest",
+    "budget": 76_000_000,
+    "revenue": 7_200_000,
+}
+
+print(
+    f"My favorite movie is {movie['title']}, which was released in {movie['year']} and was directed by {movie['director']}.\n"
+)
 
 # 2) On the lines after that, add keys to the movie dictionary for budget and revenue (you'll use code like movie['budget'] = 1000), and print out the difference between the two.
-
 # 3) If the movie cost more to make than it made in theaters, print "That was a bad investment". If the film's revenue was more than five times the amount it cost to make, print "That was a great investment." Otherwise print "That was an okay investment."
+
+delta = movie["budget"] - movie["revenue"]
+K = 1000
+M = 1_000_000
+
+if delta > 0:
+    print(
+        f"{movie['title']} lost {(delta / M)} million dollars, which made it a terrible investment, and {movie['director']} never worked in Hollywood again.\n"
+    )
+elif delta > (movie["budget"] * 5):
+    print(
+        f"{movie['title']} made {abs(delta / M),.1} million dollars in profit, and that was a great investment.\n"
+    )
+else:
+    print(
+        f"{movie['title']} made {abs(delta / M),.1} million dollars in profit, and that was a good investment.\n"
+    )
 
 # 4) Sometimes dictionaries are used to describe the same aspects of many different objects. Make ONE dictionary that describes the population of the boroughs of NYC. Manhattan has 1.6 million residents, Brooklyn has 2.6m, Bronx has 1.4m, Queens has 2.3m and Staten Island has 470,000. (Tip: keeping it all in either millions or thousands is a good idea)
 
+# NYC population in millions.
+nyc = {
+    "Manhattan": 1.6,
+    "Brooklyn": 2.6,
+    "Bronx": 1.4,
+    "Queens": 2.3,
+    "Staten Island": 0.47,
+}
+
 # 5) Display the population of Brooklyn.
+print(f"Brooklyn has a population of {nyc['Brooklyn']} million people.\n")
 
 # 6) Display the combined population of all five boroughs.
+nyc_sum = sum(nyc.values())
+print(f"New York City has a population of {nyc_sum} million people.\n")
 
 # 7) Display what percent of NYC's population lives in Manhattan.
+print(
+    f"Approximately {(nyc['Manhattan'] / nyc_sum * 100):0.2f}% of New Yorkers live in Manhattan."
+)
 
 # PART ONE: Tips
 
