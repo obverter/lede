e00ea4dc0658ecd5e32f2d2391391faad2f030a8
obverter <ben@obverter.com>
2022-06-08 15:21:42 -0700

    Init parts 2, 3

---
+++ homework/homework-2-part2-elliott.py
@@ -0,0 +1,32 @@
+# HOMEWORK PART TWO
+# Please create a new file named homework-2-part2-lastname.py
+# The first line should be a comment with your full name
+# The second line should be the date
+# The third line should be a comment "Homework 2, Part 2"
+
+# PART TWO: Lists
+
+# 1) Make a list called "countries" - it should contain seven different countries and NOT be in alphabetical order
+# 2) Using a for loop, print each element of the list
+# 3) Sort the list permanently.
+# 4) Display the first element of the list.
+# 5) Display the second-to-last element of the list.
+# 6) Delete one of the countries from the list using its name.
+# 7) Using a for loop, print each country's name in all caps.
+
+# PART TWO: Dictionaries
+
+# These will require LATITUDE and LONGITUDE. You can ask Google for latitude and longitude by typing in *coordinates of CITYNAME*, or use any other tool that exists online.
+
+# Store the latitude and longitude without the N/S/E/W - if the latitude is S, make it negative. If the longitude is W, make it negative. (See here for explanation (Links to an external site.))
+
+# 1) Make a dictionary called 'tree' that responds to 'name', 'species', 'age', 'location_name', 'latitude' and 'longitude'. Pick a tree from: https://en.wikipedia.org/wiki/List_of_trees
+# 2) Print the sentence "{name} is a {years} year old tree that is in {location_name}"
+# 3) The coordinates of New York City are 40.7128° N, 74.0059° W. Check to see if the tree is south of NYC, and print "The tree {name} in {location} is south of NYC" if it is. If it isn't, print "The tree {name} in {location} is north of NYC"
+# 4) Ask the user how old they are. If they are older than the tree, display "you are {XXX} years older than {name}." If they are younger than the tree, display "{name} was {XXX} years old when you were born."
+
+# PART TWO: Lists of dictionaries
+
+# 1) Make a list of dictionaries of five places across the world - (1) Moscow, (2) Tehran, (3) Falkland Islands, (4) Seoul, and (5) Santiago. Each dictionary should include each city's name and latitude/longitude (see note above).
+# 2) Loop through the list, printing each city's name and whether it is above or below the equator (How do you know? Think hard about the latitude.). When you get to the Falkland Islands, also display the message "The Falkland Islands are a biogeographical part of the mild Antarctic zone," which is a sentence I stole from Wikipedia.
+# 3) Loop through the list, printing whether each city is north of south of your tree from the previous section.
---
+++ homework/homework-2-part3-elliott.py
@@ -0,0 +1,40 @@
+# HOMEWORK PART THREE
+# Visit the following URL, then download the file by either clicking Download ZIP on the top right-hand corner or Raw on the right-hand side.
+
+# https://gist.github.com/jsoma/8cd1b74392975201705caf52f8fd0911 (Links to an external site.)
+
+# Download the file and save it as homework-2-part3-lastname.py
+# The first line should be a comment with your full name
+# The second line should be the date
+# The third line should be a comment "Homework 2, Part 3"
+
+
+# PART THREE: Applying your skills
+
+# It's more Spotify data, hooray! Explore it the same way we did in class - if you see a dictionary, examine its keys. Pick a key that might be useful, then go inside. If you see a list, take a look at the first element of the list. Keep going until you find what you're looking for.
+
+# Some parts of this one are VERY difficult, and some are easier. They aren't in order, so feel free to skip around! I also recommend going through the Little Columns tutorial a few times to make sure you're 100% on lists and dictionaries.
+
+# 1. How many songs are there?
+
+# 2. List the name of each song, along with its popularity.
+
+# 3. How long would it take, in minutes, to listen to all of these songs in a row?
+
+# 4. For each song, list every artist that worked on it.
+
+# 5. For each song, list every musician that worked on it EXCEPT Drake
+
+# 6. How many songs are from albums, and how many are from singles?
+
+# 7. What percentage of these songs are marked as explicit?
+
+# 8. I'd like to listen to one of the songs! Is there maybe a URL where I can listen to it?
+
+# 9. What is the most popular song?
+
+# PART THREE: Tips
+
+# Is something a dictionary? Look at the keys! Is something a list? Look at the first one or use a for loop!
+
+# You'll probably want to use for loops, and continually add to a variable you're using for counting or totaling, like in http://littlecolumns.com/learn/python/counting-with-loops/. But you might need to be more specific than you think: let's say I'm telling a robot to count the number of brown eggs in my egg basket. Robots are dumb, so it's important that I tell it we're starting with 0 eggs.
